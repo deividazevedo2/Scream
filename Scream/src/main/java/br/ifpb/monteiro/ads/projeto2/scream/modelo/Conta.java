67964db89c641dd147607b4cc817f1a69476c518
Hugo <hhuggo.correia@gmail.com>
2015-04-15 11:15:34 -0300

    Configuração e Inicialização dos testes de Unidade e Aceitação

---
+++ Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/modelo/Conta.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ifpb.monteiro.ads.projeto2.scream.modelo;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Conta {
+    
+}
---
+++ Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/teste/aceitacao/TestesAceitacao.java
@@ -0,0 +1,20 @@
+package br.ifpb.monteiro.ads.projeto2.scream.teste.aceitacao;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author Hugo Correia
+ */
+public class TestesAceitacao {
+    
+    public void testLogin(){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("localhost:8080/Scream");
+        WebElement elemento;
+        
+    }
+    
+}
