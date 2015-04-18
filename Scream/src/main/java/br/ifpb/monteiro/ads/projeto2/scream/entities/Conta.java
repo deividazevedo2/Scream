92ea1cf22dc84a8a609d0ae591a7064bcd8c3eef
Mauricio <Mauricio@Eliatrop>
2015-04-18 14:49:14 -0300

    Normalização Atualização : Conta 		UsuarioProjeto

--- Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/entities/Conta.java
+++ Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/entities/Conta.java
@@ -6,10 +6,104 @@
 
 package br.ifpb.monteiro.ads.projeto2.scream.entities;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
- *
- * @author Hugo
+ * Classe para modelar e comportar a entidade
+ * @author Mauricio
  */
-public class Conta {
+@Entity
+@Table(name = "conta")
+@PrimaryKeyJoinColumn(name = "id")
+public class Conta implements Identifiable {
+    @Id
+    @SequenceGenerator(name="id_conta",
+                       sequenceName="id_conta",
+                       allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="id_conta")
+    @Column(name = "id", updatable=false)
+    private Long id;
     
+    @OneToMany(mappedBy="id_usuario_projeto") 
+    private List<UsuarioProjeto> listUsuarioProjeto;
+          
+    @Column(nullable=false, length=30, name = "nome")
+    private String nome;
+    
+    @Column(nullable=false, length=16, name = "senha")
+    private String senha;
+    
+    @Column(nullable=false, length=10, name = "usuario")
+    private String usuario;
+    
+    @Column(nullable=false, length=40, name = "email")
+    private String email;
+
+    /**
+     *
+     * @return id
+     */
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<UsuarioProjeto> getListUsuarioProjeto() {
+        return listUsuarioProjeto;
+    }
+
+    public void setListUsuarioProjeto(List<UsuarioProjeto> listUsuarioProjeto) {
+        this.listUsuarioProjeto = listUsuarioProjeto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+
 }
--- Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/entities/UsuarioProjeto.java
+++ Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/entities/UsuarioProjeto.java
@@ -5,10 +5,75 @@
  */
 package br.ifpb.monteiro.ads.projeto2.scream.entities;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
  *
  * @author Mauricio
  */
-public class UsuarioProjeto {
+@Entity
+@Table(name = "usuario_projeto")
+@PrimaryKeyJoinColumn(name = "id")
+public class UsuarioProjeto implements Identifiable {
+    @Id
+    @SequenceGenerator(name="id_usuario_projeto",
+                       sequenceName="id_usuario_projeto",
+                       allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="id_usuario_projeto")
+    @Column(name = "id")
+    private Long id;
     
+    @ManyToOne @JoinColumn(name="id_usuario_projeto")
+    private UsuarioProjeto usuarioProjeto;
+    
+    @Column(nullable=false, length=30, name = "horario")
+    private Timestamp horario;
+    
+    @Column(nullable=false, length=30, name = "admin")
+    private Boolean admin;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UsuarioProjeto getUsuarioProjeto() {
+        return usuarioProjeto;
+    }
+
+    public void setUsuarioProjeto(UsuarioProjeto usuarioProjeto) {
+        this.usuarioProjeto = usuarioProjeto;
+    }
+
+    public Timestamp getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Timestamp horario) {
+        this.horario = horario;
+    }
+
+    public Boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }    
+
 }
