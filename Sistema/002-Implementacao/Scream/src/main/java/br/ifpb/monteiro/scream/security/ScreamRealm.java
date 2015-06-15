0d91bdb94ead76d1f98d6283f0d3bc5c8bf63c57
DarthHugh <hhuggo.correia@gmail.com>
2015-06-15 17:06:59 -0300

    Iniciação do controle de acesso ScreamRealm novo método de autorização

--- Sistema/002-Implementacao/Scream/src/main/java/br/ifpb/monteiro/scream/security/ScreamRealm.java
+++ Sistema/002-Implementacao/Scream/src/main/java/br/ifpb/monteiro/scream/security/ScreamRealm.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -66,16 +67,52 @@ public class ScreamRealm extends AuthorizingRealm {
 		return info;
 	}
 
+	// Não sei se roda 0.0
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principal) {
+		EntityManager em = emf.createEntityManager();
 		String username = (String) principal.getPrimaryPrincipal();
-		Set<String> roles = new HashSet<String>();;
+		Set<String> roles = new HashSet<String>();; // Isso n sairia null???
 		SimpleAuthorizationInfo authorization = new SimpleAuthorizationInfo();
 		SecurityService ss = new SecurityService();
 		Conta conta = ss.getConta(username);
-		//			authorization.addStringPermission(IdeenProprietaPermissions.READ_WRITE.toString());
+	  //authorization.addStringPermission(IdeenProprietaPermissions.READ_WRITE.toString());
 		authorization.setRoles(roles);
 		//resgatar da base de dados as permissões e verificar se ele tem acesso ao recurso
-		return authorization;
+		
+		//Isso deveria retornar as roles do usuário referente a conta.
+		/*
+		 * ##########IMPORTANTE##########
+		 * Depois precisamos adicionar projeto aqui. Pq cada conta pode 
+		 * ter permissões diferentes para cada projeto
+		 */
+		Query q = em.createQuery("select usuario_projeto.roles from Conta conta, UsuarioProjeto usuario_projeto "
+				+ "where conta.usuario = ?1 AND conta.id = usuario_projeto.id_conta");
+		q.setParameter(1, username);
+		
+		List<String> role = q.getResultList();
+		List<String> rol = (List<String>) authorization.getRoles();
+		
+		if (role.get(0).equals(rol.get(0))) {
+			return authorization;
+		}
+		
+		return null;
 	}
+	
+//    @Override
+//    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+//    	Conta conta = (Conta) principalCollection.fromRealm(getName()).iterator().next();
+//        // find user by login name
+//        Long uid = conta.getId();
+//        try {
+//            SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+//            for (Role role : roleDao.getRoleByUid(uid)) {
+//                info.addRole(role.getRole());
+//            }
+//            return info;
+//        } catch (BaseException ex) {
+//            return null;
+//        }
+//    }
 }
